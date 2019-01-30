3e553483f1504e03625c4e4cf74b44e90019f7ca
Piyush Bangar <piyushbangar39@gmail.com>
2019-01-30 13:39:37 +0530

    3rd commit
    date- 30 Jan, 2019

--- javascript.js
+++ javascript.js
@@ -1,15 +1,18 @@
+// Read more Button
 function show() {
-    var openbutton = document.getElementById("opener");
-    if (document.getElementById('benefits').style.display == 'none') {
-
-        document.getElementById('benefits').style.display = 'block';
-        document.getElementById('opener').style.display = 'none';
+    let openbtn = document.getElementById("opener");
+    let extradesc = document.getElementById("description");
+    if (openbtn.innerHTML == "Read More") {
+        extradesc.style.maxHeight = "150px";
+        openbtn.innerHTML = "Show Less";
+    } else if (openbtn.innerHTML = "Show Less") {
+        extradesc.style.maxHeight = "65px";
+        openbtn.innerHTML = "Read More";
     }
-    return false;
 }
 
+// Pages in Navbar
 function showhidenav() {
-    // console.log("hello");
     let pagesNav = document.getElementById("pages-nav");
     if (pagesNav.style.display === "none")
         pagesNav.style.display = "block";
@@ -23,42 +26,8 @@ window.addEventListener("click", function(e) {
         pagesNav.style.display = "none";
 });
 
-var i = 0;
-var ids = [];
-var time = 5000;
-var j = 0;
-ids[0] = "a1";
-ids[1] = "a2";
-ids[2] = "a3";
 
-function changeImgto(k) {
-    document.getElementById(ids[j]).style.display = 'none';
-    document.getElementById(ids[i]).style.display = 'none';
-    setTimeout(document.getElementById(ids[k]).style.display = 'block', time);
-
-    if (k < ids.length - 1) {
-        i = k + 1;
-    } else {
-        i = 0;
-    }
-
-}
-
-function changeImg() {
-    document.getElementById(ids[j]).style.display = 'none'
-    document.getElementById(ids[i]).style.display = 'block';
-    j = i;
-    if (i < ids.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-
-    setTimeout('changeImg()', time);
-}
-window.onload = changeImg;
-
-
+// Modal
 function imageShow(event) {
     document.getElementById('id01').style.display = 'block';
     document.getElementById("modal-image").src = event.target.src;
@@ -70,6 +39,8 @@ function hideModal() {
     document.getElementsByTagName("body")[0].style.overflow = "auto";
 }
 
+
+// Scroll to top
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
@@ -83,4 +54,103 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+
+//Ajax for form submission
+function loadText() {
+    var xhr = new XMLHttpRequest();
+    var url = "/resume";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+
+    xhr.onload = function() {
+        let response = JSON.parse(xhr.responseText);
+        if (response.success === "false") {
+            alert(response.error);
+        } else {
+            alert("Form was sucessfully submitted");
+        }
+    }
+
+    let form = "";
+    form += "name=" + document.getElementsByName("Name")[0].value + "&";
+    form += "email=" + document.getElementsByName("Email")[0].value + "&";
+    form += "phone=" + document.getElementsByName("Phone")[0].value + "&";
+    form += "message=" + document.getElementsByName("Message")[0].value;
+    xhr.send(form);
+    clearForm();
+}
+
+function clearForm() {
+    document.getElementById('inp-form-name').value = "";
+    document.getElementById('inp-form-email').value = "";
+    document.getElementById('inp-form-phone').value = "";
+    document.getElementById('inp-form-message').value = "";
+}
+document.getElementById("submit-form").addEventListener('click', loadText);
+
+
+// Carousal
+var slideIndex = 1;
+var prev_slide = 0;
+var slides;
+var dots;
+
+function showSlides() {
+    if (slideIndex > slides.length - 1) { slideIndex = 0 }
+    if (prev_slide > -1) {
+        slides[prev_slide].style.display = "none";
+        dots[prev_slide].className = dots[prev_slide].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+    prev_slide = slideIndex;
+    slideIndex++;
+}
+
+function currentSlide(index) {
+    if (index > slides.length) { index = 0 } else if (index < 0) { index = slides.length - 1 }
+
+    slides[prev_slide].style.display = "none";
+    dots[prev_slide].className = dots[prev_slide].className.replace(" active", "");
+
+    slides[index].style.display = "block";
+    dots[index].className += " active";
+    prev_slide = index;
+    slideIndex = index + 1;
+    clearInterval(Interval);
+    Interval = setInterval(showSlides, 2000);
+}
+
+function initialize_slides(cls_name, prnt_name) {
+    slides = document.getElementsByClassName(cls_name);
+    if (slides.length > 1) {
+        var db = document.createElement("div");
+        db.classList.add("dot-box");
+        document.getElementsByClassName(prnt_name)[0].appendChild(db);
+
+        for (let j = 0; j < slides.length; j++) {
+            var d = document.createElement("span");
+            d.classList.add("dot");
+            // d.attributes('index', j);
+            db.appendChild(d);
+            d.addEventListener('click', currentSlide.bind(null, j))
+        }
+
+        dots = document.getElementsByClassName("dot");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[0].style.display = "block";
+        dots[0].className += " active";
+        Interval = setInterval(showSlides, 2000);
+    }
+}
+
+let Interval;
+window.onload = function() {
+    initialize_slides("grid-container", "slideshow-container");
 }
